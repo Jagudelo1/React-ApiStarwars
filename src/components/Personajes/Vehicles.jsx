da8ff0420675cf9ddf8f4fe8fc2bfea068e69d5c
Juan David Agudelo <93266129+Jagudelo1@users.noreply.github.com>
2023-09-22 17:03:59 -0500

    ...

---
+++ src/components/Personajes/Vehicles.jsx
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+import { Card } from "react-bootstrap";
+import "../../css/Detalles.css";
+
+export const Vehicles = ({url}) => {
+    const [ data3, setData3 ] = useState([]);
+
+    useEffect(() => {
+        fetch(url).then(res => res.json()).then(data3 => setData3(data3))
+    }, [url])
+
+    return(
+        <div className="DetallesContainer">
+            <Card style={{ width: '21rem'}}>
+                <Card.Body>
+                    <Card.Text>
+                        Nombre: <span>{data3.name}</span>
+                    </Card.Text>
+                    <Card.Text>
+                        Modelo: <span>{data3.model}</span>
+                    </Card.Text>
+                    <Card.Text>
+                        Fabricante: <span>{data3.manufacturer}</span>
+                    </Card.Text>
+                    <Card.Text>
+                        Pasajeros: <span>{data3.passengers}</span>
+                    </Card.Text>
+                    <Card.Text>
+                        Capacidad Máxima: <span>{data3.cargo_capacity}</span>
+                    </Card.Text>
+                    <Card.Text>
+                        Clase de Vehiculo: <span>{data3.vehicle_class}</span>
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+        </div>
+    )
+}
